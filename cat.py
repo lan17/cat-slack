c19e7133afd059b4ffdca23cd4a26e018ad676d6
Lev Alexandrovich Neiman <lev.neiman@gmail.com>
2019-05-15 23:49:52 -0700

    polish

--- cat.py
+++ cat.py
@@ -3,17 +3,29 @@
 import getpass
 import os
 import random
+import time
 
 import slack
 
-token = getpass.getpass("Slack token:")
 
-client = slack.WebClient(token=token)
+def main():
+    token = getpass.getpass("Slack token:")
 
-kitty_files = list(filter(lambda x: x.endswith('png'), os.listdir('.')))
+    while True:
+        client = slack.WebClient(token=token)
 
-print(kitty_files)
-random_kitty = kitty_files[random.randint(0, len(kitty_files) - 1)]
-print("Chosen kitty " + random_kitty)
+        kitty_files = list(filter(lambda x: x.endswith('png'), os.listdir('.')))
+        print('Picking kitty out of ' + str(kitty_files))
+        random_kitty = kitty_files[random.randint(0, len(kitty_files) - 1)]
 
-# client.users_setPhoto(image="./1.png")
+        print('Chosen kitty ' + random_kitty)
+
+        client.users_setPhoto(image=os.path.join('.', random_kitty))
+
+        print("Updated the photo.  Meow!")
+
+        time.sleep(3600 * 24)
+
+
+if __name__ == '__main__':
+    main()
